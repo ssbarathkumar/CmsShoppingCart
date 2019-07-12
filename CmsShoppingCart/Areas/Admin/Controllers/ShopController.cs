4726174e08e6c84dece9ec05a916212b1126c5d6
ssbarathkumar05 <ssbarathkumar05@gmail.com>
2019-07-12 23:38:40 +0530

    Rename Category

--- CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
@@ -26,5 +26,120 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             //Return View with List
             return View(categoryVMList);
         }
+
+        //POST: Admin/Shop/AddNewCategory
+
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+
+            //Declare id
+            string id;
+
+            using (Db db = new Db())
+            {
+
+                //Check the category name is unique
+                if(db.Categories.Any(x=>x.Name == catName))
+                {
+                    return "titletaken";
+                }
+
+                //Initialize DTO
+                CategoryDTO dto = new CategoryDTO();
+
+                //Add to DTO
+                dto.Name = catName;
+                dto.Slug = catName.Trim().Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+
+                //save DTO
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                //Get the id
+                id = dto.Id.ToString();
+            }
+
+            //Return the id
+
+            return id;
+        }
+
+        //POST: Admin/Shop/ReorderCategories
+
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+
+                //set intial count
+                int cout = 1;
+
+                //Declare CatergoryDTO
+                CategoryDTO dto;
+
+                //set sort for each page
+                foreach (var catid in id)
+                {
+                    dto = db.Categories.Find(catid);
+                    dto.Sorting = cout;
+
+                    db.SaveChanges();
+
+                    cout++;
+                }
+            }
+        }
+
+
+        //GET : Admin/Shop/DeleteCategory/id
+
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Db db = new Db())
+            {
+
+                //Get Category
+                CategoryDTO dto = db.Categories.Find(id);
+
+                //Remove Page
+                db.Categories.Remove(dto);
+
+                //Save
+                db.SaveChanges();
+
+            }
+
+            //redirect
+            return RedirectToAction("Categories");
+        }
+
+        //POST: Admin/Shop/ReorderCategories
+        [HttpPost]
+        public string RenameCategory(string newCatName,int id)
+        {
+            using (Db db = new Db())
+            {
+                //check category name unique
+                if (db.Categories.Any(x => x.Name == newCatName))
+                    return "titletaken";
+
+                //get DTO
+                CategoryDTO dto = db.Categories.Find(id);
+
+                //Edit DTO
+                dto.Name = newCatName;
+                dto.Slug = newCatName.Replace(" ", "-").ToLower();
+
+                //Save
+                db.SaveChanges();
+            }
+
+            //Return
+            return "success";
+        }
+
     }
 }
